ad6945d18b9dcbd514fcbd8d963449b77350a625
Dilisha <dilishamihiruth@gmail.com>
2022-12-28 18:35:08 +0530

    Remove bootstrap code and add css file

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,21 +1,17 @@
 @extends('header')
 @section('content')
 
+<link rel="stylesheet" href="/css/loginstyle.css"/>
 
-<div class="row justify-content-center">
+    
 
-    <div class="col-xl-10 col-lg-12 col-md-9">
-
-        <div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
-                <!-- Nested Row within Card Body -->
-                <div class="row">
-                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                    <div class="col-lg-6">
-                        <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                            </div>
+   
+                     <div class="wrapper">
+                            
+                                
+                            
+                               <h2>Login</h2>
+                            
 
                             @if (session('error'))
                                 <span class="text-danger"> {{ session('error') }}</span>
@@ -23,8 +19,8 @@
 
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
-                                <div class="form-group">
-                                    <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Enter Email Address.">
+                                <div class="input-field">
+                                    <input id="email" type="email" class="input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Enter Email Address.">
 
                                         @error('email')
                                             <span class="invalid-feedback" role="alert">
@@ -32,8 +28,8 @@
                                             </span>
                                         @enderror
                                 </div>
-                                <div class="form-group">
-                                    <input id="password" type="password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
+                                <div class="input-field">
+                                    <input id="password" type="password" class="input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -41,39 +37,36 @@
                                         </span>
                                     @enderror
                                 </div>
-                                <div class="form-group">
-                                    <div class="custom-control custom-checkbox small">
-                                        <input class="custom-control-input" type="checkbox" name="remember" id="customCheck" {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="custom-control-label" for="customCheck">Remember
-                                            Me</label>
+                                <div class="chekbox-text">
+                                    <div class="RememberMeinput">
+                                        <input class="check-box" type="checkbox" name="remember" id="customCheck" {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="RememberMe-Text" for="customCheck">Remember Me</label>
                                     </div>
                                 </div>
-                                <button class="btn btn-primary btn-user btn-block">
+                                <button >
                                     Login
                                 </button>
-                                <hr>
-                                <a href="{{ route('facebook.login') }}" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                <div class="line"></div>
+                                <div class="media-option">
+                                    <a href="{{ route('facebook.login') }}" class="facebook">
+                                        <img src="img/facebook.png" class="facebook-img"> Login with Facebook
+                                    </a>
+                                </div>
+                                <div class="media-option">
+                                <a href="{{ route('google.login') }}" class="google">
+                                    <img src="img/google.png" class="google-img"> Login with Google
                                 </a>
-                                <a href="{{ route('google.login') }}" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Login with Google
-                                </a>
+                                </div>
                             </form>
-                            <hr>
+                            
 
                             <div class="text-center">
                                 <a class="small" href="{{('register')}}">Create an Account!</a>
                             </div>
-                        </div>
+                        
                     </div>
-                </div>
-            </div>
-        </div>
+                    
 
-    </div>
-
-</div>
 
 
         
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -2,49 +2,46 @@
 
 @section('content')
 
+<link rel="stylesheet" href="/css/loginstyle.css"/>
 
-
-<div class="container mt-5">
-    <div class="row d-flex justify-content-center">
-    <div class="col-md-6">
-   
-        <div class="card">
-            <div class="card-header">Registration</div>
-            <div class="card-body">
+<div class="wrapper1">
+    
+            <h2>Registration</h2>
+            <div class="">
                 <form action="{{route('user.validate_registration')}}" method="POST">
                     @csrf
-                    <form class="row g-3">
-                        <div class="col-md-6">
-                            <label for="name">Username</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Username">
+                    <form class="">
+                        <div class="">
+                            
+                            <input type="text" class="inputRegistraion" id="name" name="name" placeholder="Enter Username">
                              @if($errors->has('name'))
                              <span class="text-danger">{{$errors->first('name')}}</span>
                              @endif
                              </div><br>
-                              <div class="col-md-6">
-                                <label for="email">Email</label>
-                                <input type="text" class="form-control" id="email" name="email" placeholder="Enter Email">
+                              <div class="">
+                                
+                                <input type="text" class="inputRegistraion" id="email" name="email" placeholder="Enter Email">
                                 @if($errors->has('email'))
                                 <span class="text-danger">{{$errors->first('email')}}</span>
                                 @endif
                             </div><br>
-                            <div class="col-md-6">
-                                <label for="password">Password</label>
-                                <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                            <div class="">
+                                
+                                <input type="password" class="inputRegistraion" name="password" id="password" placeholder="Password">
                                 @if($errors->has('password'))
                                 <span class="text-danger">{{$errors->first('password')}}</span>
                                 @endif
                             </div>
                             <br>
-                            <div class="col-12">
-                                <button type="submit" class="btn btn-primary">Signup</button>
+                            <div class="">
+                                <button type="submit" class="signupbtn">Signup</button>
                             </div>
                         </form>
                     </div>
                
-            </div>
-        </div>
-    </div>
+            
+        
+    
 </div>
 
       
